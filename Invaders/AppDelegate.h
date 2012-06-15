da2e972ce0610f0fb9b94fead4dfbbcd81c71971
Yangfan Qi <reminisce214@Yangfan-Qis-MacBook-Pro.local>
2012-06-15 10:50:49 -0700

    Initial Commit

---
+++ Invaders/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Invaders
+//
+//  Created by Yangfan Qi on 6/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
